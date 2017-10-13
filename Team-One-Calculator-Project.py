83135fe39a4191e49bc93b93a19e93c23c7c6df6
GhulamNabi76 <32115151+GhulamNabi76@users.noreply.github.com>
2017-10-12 21:51:20 -0400

    Calculator source file.
    Calculator source file.

---
+++ Team-One-Calculator-Project.py
@@ -0,0 +1,43 @@
+# FILE NAME : Team-One-Calculator-Project.py
+# DESCRIPTION : Calculator Program
+# FUNCTION DEFINITIONS:
+
+def div(n1,n2):
+ return n1/n2
+
+def mult(n1,n2):
+ return (n1*n2)
+
+def sub(n1, n2):
+ return (n1-n2)
+
+def add(num1,num2):
+ c=num1+num2
+ print ("Sum = " + str(c))
+
+# INITIAL MESSAGE:
+print ("Welcome to Calculator Function")
+print ("Menu:")
+print ("1. Add")
+print ("2. Subtract")
+print ("3. Multiply")
+print ("4. Division")
+choice = input("Enter choice (1/2/3/4): ")
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+
+if choice is 2:
+ c = sub(num1, num2)
+ print (c)
+
+if(choice is 1):
+ add(num1,num2)
+
+if(choice is 3):
+ c = mult(num1,num2)
+ print (c)
+
+if choice is 4:
+ c=div(num1,num2)
+ print (c)
+ x = input("dekh le output")
